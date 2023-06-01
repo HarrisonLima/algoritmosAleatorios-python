a1fa558b75089ab29c1bc647ee0899c7ac4f5ed5
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2023-05-31 22:06:58 -0300

    Adicionando exercicio 11

---
+++ main.py
@@ -0,0 +1,12 @@
+def SomaQuadrados():
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor: "))
+    valor3 = int(input("Digite o terceiro valor: "))
+
+    resultado = valor1 + valor2 + valor3
+
+    input(f'Resultado: { resultado:.2f }')
+
+if __name__ == '__main__':
+    SomaQuadrados()
+
