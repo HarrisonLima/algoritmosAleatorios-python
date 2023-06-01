8f68d6d2479d2d1eda7625c5527bad9a006113e6
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2023-05-31 22:24:53 -0300

    Adicionando exercicios 12 e 13. Estruturado arquivo main com o switch de exercicio

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
-def SomaQuadrados():
-    valor1 = int(input("Digite o primeiro valor: "))
-    valor2 = int(input("Digite o segundo valor: "))
-    valor3 = int(input("Digite o terceiro valor: "))
+from email.policy import default
 
-    resultado = valor1 + valor2 + valor3
+from src.exercicio11 import somaQuadrados
+from src.exercicio12 import quadradoSoma
+from src.exercicio13 import velocidadeProjetil
 
-    input(f'Resultado: { resultado:.2f }')
+print("----------Listagem de exercícios----------")
+print("A1. Two Fer (2-Fer)")
+print("A2. Troca")
+print("A3. Ajuste Salarial")
+print("A4. FizzBuzz")
+print("A5. Área de uma circunferência")
+print("A6. Soma dos pares)")
+print("A7. Salário Líquido Professor")
+print("A8. Volume da caixa")
+print("A9. Conversão em Real")
+print("A10. Conversão em Dólar")
+print("A11. Soma dos Quadrados")
+print("A12. Quadrado da Soma")
+print("A13. Velocidade do Projétil")
+print("A14. Eleição Sindical")
+print("A15. Média do Aluno")
+print("A16. Somando Naturais")
+print("A17. Ordenando Inteiros")
+print("A18. Palíndromo")
+print("A19. Scrabble Score")
+print("A20. Fatorial")
+print("A21. Algarismos Romanos")
+print("A22. Primo")
+print("A23. JoKenPo ")
+print("A24. Pedra-papel-tesoura-lagarto-Spock")
+print("A25. Soma dos pares")
+print("A26. Anagramas")
+print("A27. Letras repetidas")
+print("A28. Fibonacci")
+print("A29. Pangrama")
+print("A30. Troco")
+print("A31. Quebra de Linha")
+print("A32. Sudoku")
+print("----------------------------------------")
+
+
+def switch_case(case):
+    switch = {
+        11: somaQuadrados.SomaQuadrados(),
+        12: quadradoSoma.QuadradoSoma(),
+        13: velocidadeProjetil.VelocidadeProjetil()
+    }
+    switch.get(case, default)()
 
 if __name__ == '__main__':
-    SomaQuadrados()
-
+    opcao = int(input("Digite o número do exercício que deseja executar: "))
+    switch_case(opcao)
--- src/exercicio12/quadradoSoma.py
+++ src/exercicio12/quadradoSoma.py
@@ -1,8 +1,9 @@
-def SomaQuadrados():
+def QuadradoSoma():
     valor1 = int(input("Digite o primeiro valor: "))
     valor2 = int(input("Digite o segundo valor: "))
     valor3 = int(input("Digite o terceiro valor: "))
 
-    resultado = valor1**2 + valor2**2 + valor3**2
+    resultado = (valor1 + valor2 + valor3)**2
+
     print(f'Resultado: {resultado}')
 
